fd554e8fa564a5170c0c7af2f0273104efc47876
Wycliffe <wycliffekiprono4@gmail.com>
2023-05-18 13:07:54 +0300

    done

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,24 @@
 def starts_with_a_vowel?(word)
-
+    /^[aeiou]/i.match?(word)
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
 
+def words_starting_with_un_and_ending_with_ing(text)
+    text.scan(/\bun\w+ing\b/i)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
+puts words_five_letters_long("The quick guide to ruby should exist")
 
+def first_word_capitalized_and_ends_with_punctuation?(text)
+ !!(text =~ /^[A-Z][\w']*\b.*[.?!]$/)
 end
 
 def valid_phone_number?(phone)
-
-end
+    !!phone.match(/^\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$/)
+  end
+  
+  
